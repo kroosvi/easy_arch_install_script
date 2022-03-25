d584e3c9785caa12a4b14efa6a2651a9cf1c00c2
kroos Xiang <kroosvi@gmail.com>
2022-03-24 20:52:31 -0400

    first commit

---
+++ .install.sh
@@ -0,0 +1,24 @@
+timedatectl set-ntp true
+
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+ 
+hwclock --systohc
+ 
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+locale-gen
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+
+pacman -S --noconfirm nvidia nvidia-utils nvidia-settings refind dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers xdg-utils
+pacman -S --noconfire rsync 
+systemctl enable NetworkManager
+systemctl enable sshd
+systemctl enable avahi-daemon
+
+
+useradd -m kroozy
+usermod -aG wheel ermanno
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
