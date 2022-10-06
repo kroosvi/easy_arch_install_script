ddfcb5f1ac1d388ae55c59fff7f17ba9ea220cc7
Tingxuan Xiang <42981577+kroosvi@users.noreply.github.com>
2022-10-06 11:55:32 -0400

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -6,19 +6,29 @@ hwclock --systohc
  
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 locale-gen
+
 echo "arch" >> /etc/hostname
+
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 
-pacman -S --noconfirm nvidia nvidia-utils nvidia-settings refind dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers xdg-utils
-pacman -S --noconfire rsync 
-systemctl enable NetworkManager
+pacman -S --noconfirm reflector
+reflector --verbose --country 'United States' -l 5 --sort rate --save /etc/pacman.d/mirrorlist
+
+pacman -S --noconfirm rsync nvidia nvidia-utils nvidia-settings
+pacman -S --noconfirm refind dialog wpa_supplicant mtools dosfstools  base-devel linux-headers xdg-utils curl
+
+pacman -S --noconfirm ranger nvim i3 polybar alacritty zsh discord flameshot firefox pulseaudio lxappearance 
+pacman -S --noconfirm yay
+
+
 systemctl enable sshd
 systemctl enable avahi-daemon
+systemctl enable NetworkManager
 
 
 useradd -m kroos
 usermod -aG wheel kroos
 
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+printf "\e[1;32mDone! Add a passwrod! Type exit, umount -a and reboot.\e[0m"
