c73daabec3810f70fa3e1047e258e780561c69d2
kroos <kroosvi@gmail.com>
2023-01-07 17:47:21 -0500

    Updated

--- arch_install.sh
+++ arch_install.sh
@@ -1,9 +1,7 @@
 timedatectl set-ntp true
-
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
- 
 hwclock --systohc
- 
+
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 locale-gen
 
@@ -16,19 +14,22 @@ echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 pacman -S --noconfirm reflector
 reflector --verbose --country 'United States' -l 5 --sort rate --save /etc/pacman.d/mirrorlist
 
-pacman -S --noconfirm rsync nvidia nvidia-utils nvidia-settings
-pacman -S --noconfirm refind dialog wpa_supplicant mtools dosfstools  base-devel linux-headers xdg-utils curl
 
-pacman -S --noconfirm ranger nvim i3 polybar alacritty zsh discord flameshot firefox pulseaudio lxappearance 
-pacman -S --noconfirm yay
+
+pacman -S --noconfirm rsync nvidia nvidia-utils nvidia-settings refind
+pacman -S --noconfirm dialog wpa_supplicant mtools dosfstools  base-devel linux-headers xdg-utils curl
+pacman -S --noconfirm ranger nvim polybar fish discord flameshot firefox pulseaudio lxappearance sshd gdm
+
+pacman -S pacman -S sway i3status swayidle swaylock alacritty rofi 
+
 
 
 systemctl enable sshd
-systemctl enable avahi-daemon
+systemctl enable gdm.service
 systemctl enable NetworkManager
 
 
-useradd -m kroos
-usermod -aG wheel kroos
+useradd -m kroozy
+usermod -aG wheel sudo kroozy
 
 printf "\e[1;32mDone! Add a passwrod! Type exit, umount -a and reboot.\e[0m"
