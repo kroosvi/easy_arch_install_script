6e8d37b02b67499742888bd8561c93f63ddc06f0
kroos <kroosvi@gmail.com>
2023-01-07 19:29:10 -0500

    working process

--- arch_install.sh
+++ arch_install.sh
@@ -1,4 +1,3 @@
-timedatectl set-ntp true
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
 
@@ -6,30 +5,33 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 locale-gen
 
 echo "arch" >> /etc/hostname
-
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
 
-pacman -S --noconfirm reflector
+#best mirrorlist for USA
+pacman -S reflector
 reflector --verbose --country 'United States' -l 5 --sort rate --save /etc/pacman.d/mirrorlist
 
 
+#system
+pacman -S networkmanager rsync dialog wpa_supplicant mtools 
+pacman -S dosfstools base-devel linux-headers xdg-utils curl ranger 
+pacman -S flameshot pulseaudio lxappearance sshd  lightdm
+pacman -S alsa-utils nvidia nvidia-utils nvidia-settings refind conky fish firefox
 
-pacman -S --noconfirm rsync nvidia nvidia-utils nvidia-settings refind
-pacman -S --noconfirm dialog wpa_supplicant mtools dosfstools  base-devel linux-headers xdg-utils curl
-pacman -S --noconfirm ranger nvim polybar fish discord flameshot firefox pulseaudio lxappearance sshd gdm
+# your own 
+pacman -S awesome alacritty rofi discord 
 
-pacman -S pacman -S sway i3status swayidle swaylock alacritty rofi 
-
-
+refind-install 
 
 systemctl enable sshd
-systemctl enable gdm.service
+systemctl enable lightdm
 systemctl enable NetworkManager
+systemctl enable pulseaudio
 
 
 useradd -m kroozy
-usermod -aG wheel sudo kroozy
+usermod -G wheel kroozy
 
 printf "\e[1;32mDone! Add a passwrod! Type exit, umount -a and reboot.\e[0m"
