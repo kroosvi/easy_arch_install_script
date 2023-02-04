431a7bb7079ba5f915d4cee0be8a141d49135a95
Kroos Xiang <42981577+kroosvi@users.noreply.github.com>
2023-02-04 10:02:25 -0500

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -4,27 +4,30 @@ hwclock --systohc
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 locale-gen
 
-echo "arch" >> /etc/hostname
+#change the user name and the host name for yourself 
+echo "1121" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
-echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo "127.0.1.1 1121.localdomain 1121" >> /etc/hosts
 
 #system
 pacman -S networkmanager rsync dialog wpa_supplicant mtools base-devel
 pacman -S dosfstools linux-headers xdg-utils curl ranger
-pacman -S flameshot pulseaudio lxappearance sshd  lightdm
-pacman -S alsa-utils nvidia nvidia-utils nvidia-settings refind fish firefox
+pacman -S flameshot pulseaudio lxappearance sshd  sddm
+pacman -S alsa-utils nvidia nvidia-utils nvidia-settings refind zsh firefox
 
 #your own applications
-pacman -S i3 alacritty rofi discord unzip p7zip dunst feh
-
-refind-install 
+pacman -S i3 alacritty rofi discord unzip p7zip dunst feh polybar
 
 systemctl enable sshd
 systemctl enable bluetooth
-systemctl enable lightdm
+systemctl enable sddm
 systemctl enable NetworkManager
 systemctl enable pulseaudio
+systemctl enable reflector.timer
+
+#add root passpwd
+echo root:password | chpasswd
 
 #add user and passwd
 useradd -m kroozy
@@ -32,4 +35,4 @@ echo kroozy:password | chpasswd
 usermod -G wheel kroozy
 echo "kroozy ALL=(ALL) ALL" >> /etc/sudoers.d/kroozy
 
-printf "\e[1;32m Please add another boot loader Then Type exit, umount -a and reboot.\e[0m"
+printf "\e[1;32m Please add another boot loader Then Type exit, umount -R /mnt and reboot.\e[0m"
